177233fa1129cf7d4cec33bc6e400af0bf18408a
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2019-01-29 14:33:31 -0600

    helloworld to test C# is working
    yaya

--- myApp/Program.cs
+++ myApp/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace myApp
+namespace list_tutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var names = new List<string> { "Mae", "Ana", "Felipe" };
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Maria");
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
         }
     }
 }
